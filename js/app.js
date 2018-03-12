f4a61f6b28f0606f8e81a7f2218253e4491a4963
Darsh <darsh2987@gmail.com>
2018-03-12 15:41:35 +0000

    modified - replaced query selectors with variables.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 document.querySelector("#hamburger").addEventListener("click", menu);
 function menu () {
-  if (document.querySelector("#nav").classList.contains("nav--reveal")) {
-    document.querySelector("#nav").classList.remove("nav--reveal");
-    document.querySelector("#hamburger").classList.remove("hamburger-container-open");
+  const nav = document.querySelector("#nav");
+  const hamburger = document.querySelector("#hamburger");
+
+  if (nav.classList.contains("nav--reveal")) {
+    nav.classList.remove("nav--reveal");
+    hamburger.classList.remove("hamburger-container-open");
   } else {
-    document.querySelector("#nav").classList.add("nav--reveal");
-    document.querySelector("#hamburger").classList.add("hamburger-container-open");
+    nav.classList.add("nav--reveal");
+    hamburger.classList.add("hamburger-container-open");
   }
 };
