e74998b32ba90d068c1a643fe28aabb84ca399b4
Darsh <darsh2987@gmail.com>
2018-04-29 13:23:46 +0100

    mouseOver fucntion, restructured code for more functionality, added comments.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
-document.querySelector("#hamburger").addEventListener("click", menu);
-function menu () {
-  const nav = document.querySelector("#nav");
-  const hamburger = document.querySelector("#hamburger");
+window.addEventListener('load', function() {
 
-  if (nav.classList.contains("nav--reveal")) {
-    nav.classList.remove("nav--reveal");
-    hamburger.classList.remove("hamburger-container-open");
-  } else {
-    nav.classList.add("nav--reveal");
-    hamburger.classList.add("hamburger-container-open");
-  }
-};
+  //Function for hamburger icon to open up the navigation menu during mobile view.
+  function hamburger () {
+    document.querySelector("#hamburger").addEventListener("click", menu);
+    function menu () {
+      const nav = document.querySelector("#nav");
+      const hamburger = document.querySelector("#hamburger");
+
+      if (nav.classList.contains("nav--reveal")) {
+        nav.classList.remove("nav--reveal");
+        hamburger.classList.remove("hamburger-container-open");
+      } else {
+        nav.classList.add("nav--reveal");
+        hamburger.classList.add("hamburger-container-open");
+      }
+    };
+  };
+
+  //Function for hover effect on artist images for all artist pages.
+  function hoverEffect () {
+    const images = document.querySelectorAll(".artist_digiblus div a img");
+    const artistName = document.querySelectorAll(".artist_digiblus div p");
+
+    for (let i = 0;  i < images.length; i++) {
+      for (let j = 0; j < artistName.length; j++) {
+        images[i].addEventListener("mouseover", hoverOn);
+        function hoverOn () {
+          artistName[i].style.background = "#1ca9d6";
+          artistName[i].style.color = "#fff"
+        }
+      }
+    }
+
+    for (let i = 0;  i < images.length; i++) {
+      for (let j = 0; j < artistName.length; j++) {
+        images[i].addEventListener("mouseout", hoverOff);
+        function hoverOff () {
+          artistName[i].style.background = "";
+          artistName[i].style.color = ""
+        }
+      }
+    }
+  };
+  hoverEffect();
+  hamburger();
+});
