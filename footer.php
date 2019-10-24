3895857e8c9b089a7353031665575815948f057c
Darsh <darsh2987@gmail.com>
2019-10-24 11:55:05 +0100

    footer.php created with markup.

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+      <!-- Social -->
+      <div class="social-links">
+        <ul>
+          <li class="facebook"><a href="<?php echo 'https://www.facebook.com/blusaphirrecs/?ref=nf'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/facebook.png" alt="Facebook"></a></li>
+          <li><a href="<?php echo 'https://www.youtube.com/user/jayrome1978'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/youtube.png" alt="YouTube"></a></li>
+          <li><a href="<?php echo 'https://soundcloud.com/blusaphir_records'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/soundcloud.png" alt="Sound Cloud"></a></li>
+          <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/twitter.jpg" alt="Twitter"></a></li>
+          <li><a href="<?php echo 'https://www.blogger.com/profile/09110695010939377220'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/blogger.png" alt="Blogger"></a></li>
+        </ul>
+      </div>
+
+      <!-- Footer -->
+      <footer>
+        <p>All content © Blu Saphir Recordings Limited 2019. All Rights Reserved.</p>
+        <p>Website designed and developed by Darshan Patel and Jason Williams.</p>
+      </footer>
+      <!-- End of Footer -->
+    </div> <!-- End of Wrapper -->
+
+    <?php wp_footer(); ?>
+  </body>
+</html> 
