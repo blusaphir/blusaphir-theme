7c2f40713c3f924161aa3682b15857006b723a93
Darsh <darsh2987@gmail.com>
2019-10-24 11:46:18 +0100

    action added for js scripts and stylesheet.

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+  function blusaphir_files() {
+    wp_enqueue_script('blusaphir_js', get_theme_file_uri('/js/app.js'));
+    wp_enqueue_style('blusaphir_main_style', get_stylesheet_uri());
+  }
+
+  add_action('wp_enqueue_scripts', 'blusaphir_files');
+
+?>
+
