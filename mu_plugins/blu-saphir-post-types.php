f25325e18175ef4415ce61b0ae4ed4c2dcf08142
Darsh <darsh2987@gmail.com>
2019-10-24 13:56:33 +0100

    Script for Sub Feature custom post type added.

--- mu_plugins/blu-saphir-post-types.php
+++ mu_plugins/blu-saphir-post-types.php
@@ -11,7 +11,20 @@ function blusaphir_post_types() {
       'all_items' => 'All Main Features',
       'singular_name' => 'Main Feature'
     )
-    ));
+  ));
+
+  // Front-Page Sub-Feature Post Type
+  register_post_type('sub_feature', array(
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Sub-Feature',
+      'add_new_item' => 'Add New Sub-Feature',
+      'edit_item' => 'Edit Sub-Feature',
+      'all_items' => 'All Sub-Features',
+      'singular_name' => 'Sub-Feature'
+    )
+  ));
 }
 
 add_action('init', 'blusaphir_post_types');
