2d2f6018f7e89d9531d25803580cdc97f1fa3afd
Darsh <darsh2987@gmail.com>
2019-11-16 17:29:26 +0000

    new custom post type added for the front page - like section.

--- mu_plugins/blu-saphir-post-types.php
+++ mu_plugins/blu-saphir-post-types.php
@@ -25,6 +25,19 @@ function blusaphir_post_types() {
       'singular_name' => 'Sub-Feature'
     )
   ));
+
+  //Front Page Like Section Post Type
+  register_post_type('like_section', array(
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Like',
+      'add_new_item' => 'Add New Like',
+      'edit_item' => 'Edit Like',
+      'all_items' => 'All Like',
+      'singular_name' => 'Like'
+    )
+  ));
 }
 
 add_action('init', 'blusaphir_post_types');
