fd802d2b09ee5d18fbd516975416e7607bbca505
Darsh <darsh2987@gmail.com>
2020-01-11 20:05:13 +0000

    release.php page added with header and footer tags and main body markup.

---
+++ release.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Release */ ?>
+
+<?php get_header() ?>
+
+      <!-- Main Feature -->
+      <section class="artist-main">
+        <a href="<?php echo site_url('/artist-digiblus'); ?>"><img class="artist-main__icon" src="<?php echo get_template_directory_uri(); ?>/images/digiblus.png" alt=""></a>
+        <a href=""><img class="artist-main__icon" src="<?php echo get_template_directory_uri(); ?>/images/blusltd.jpg" alt=""></a>
+        <a href=""><img class="artist-main__icon" src="<?php echo get_template_directory_uri(); ?>/images/syncsaphir.png" alt=""></a>
+        <a href=""><img class="artist-main__icon" src="<?php echo get_template_directory_uri(); ?>/images/blusaphir2.png" alt=""></a>
+      </section>
+
+<?php get_footer() ?>
