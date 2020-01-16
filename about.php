62dbad4315ec483accf6a4da6ca830878e1ecaec
Darsh <darsh2987@gmail.com>
2020-01-16 16:34:04 +0000

    removed hard code and replaced with post loop.

--- about.php
+++ about.php
@@ -2,45 +2,23 @@
 
 <?php get_header() ?>
 
-      <!-- Main Feature -->
-      <section class="about">
-        <div class="about-feature about-feature-one">
-          <img class="about-feature__img" src="assets/images/blusaphir.jpg" alt="">
-          <div class="content-wrapper">
-            <h2 class="about-feature__heading">Blu Saphir</h2>
-            <p class="about-feature__info">Blu Saphir Recordings was created in 2005 on the grounds of improving and expanding the variety of sounds that drum and bass has to offer. Jay Rome got in contact with Muffler, Kaleb, and Contour back in 2004. He loved their music and decided to work with them on a new label to release proper music. “Twintone and I had completed a tune entitled I Try, which had such good feedback that Muffler decided to do a remix. We had these tunes ready for the label, so we started Blu Saphir Recordings to release it.”
+<section class="about">
+  <?php 
+    $about = new WP_Query(array(
+      'post_type' => 'about'
+    ));
 
-            Contour, Twintone, DJ Rome (the label owner's moniker), and Kaleb went on to produce two successful releases – Contour on Synapse and Twintone & DJ Rome on I Try for release number one; followed by a duo produced by Kaleb & Contour for BLUS002 – Straight Ahead and Hold My Love.
+    while($about->have_posts()) {
+      $about->the_post(); ?>
 
-            Following the immense attention these two releases received, the third release gave the remix treatment to the first release – Muffler reworking I Try and Bungle rearranging Synapse. Blu Saphir continued acquiring an A-list of talent for its roster, including Aspect, Electrosoul System, DK Foyer & Jebar, Chino, Submorphics, The Ego, DJ Roots, Peyo, Cloud Nine, Soul Mozaics and W.T.</p>
-          </div>
-        </div>
-
-        <div class="about-feature about-feature-two">
-          <img class="about-feature__img" src="assets/images/blusltd.jpg" alt="">
-          <div class="content-wrapper">
-            <h2 class="about-feature__heading">Blu Saphir Limited</h2>
-            <p class="about-feature__info">As evolution would have it, the force behind the Blu Saphir camp wasn't done – 2013 dawned a new era of techy, deep, dark and rolling drum 'n bass in the amalgamation of Blu Saphir LTD. Introducing a pair of thunderous rollers by Japan's Hayato, the imprint has gone on to release bangers by DJ Snare, Monologue, DJ Chap, Roy Green & Protone, and NME Click. Up & coming release by DJ Chap & DJ Snare.</p>
-          </div>
-        </div>
-
-        <div class="about-feature about-feature-three">
-          <img class="about-feature__img" src="assets/images/digiblus.png" alt="">
-          <div class="content-wrapper">
-            <h2 class="about-feature__heading">Digital Blus</h2>
-            <p class="about-feature__info">As Blu Saphir matured, Digital Blus was formed three years later in 2008 with the mission in mind to release the highest quality drum 'n bass in digital format. The focus was shifted to cater to upcoming producers putting out quality releases that have yet to see a vinyl release. The digital label was widely accepted and praised by the likes of Chrissy Chris (1XTRA), DJ Marky, Fabio, Ash Attack, Random Movement, Specific, Submorphics and many more. From this day on, all Blu Saphir tunes would be available through Beatport, iTunes, Finetunes, and Trackitdown.</p>
-          </div>
-        </div>
-
-        <div class="about-feature about-feature-four">
-          <img class="about-feature__img" src="assets/images/syncsaphir.png" alt="">
-          <div class="content-wrapper">
-            <h2 class="about-feature__heading">Sync:Saphir</h2>
-            <p class="about-feature__info">After numerous successful releases on both labels, label owners, Jay Rome of Blu Saphir and Syncopix of (you guessed it!) Syncopix Recordings have combined forced to create a new collaboration, Sync:Saphir.
-
-            “The goal behind Sync:Saphir is to push out some fresh minimal, deep and liquid drum and bass music by both talent new artists and well known producers,” Jay Rome of Blu Saphir goes on - “At first , I had the idea to release an EP with Syncopix, but then we decided to make a bigger wave with an LP and two sampler vinyls. The  SYNC:SAPHIR LP will be a double CD album.  On the other CD we will present 13 tracks by both well known and fresh newcomer producers.”</p>
-          </div>
-        </div>
-      </section>
+      <div class="content-wrapper">
+        <h2><?php the_title(); ?></h2>
+        <?php the_post_thumbnail(); ?>
+        <p><?php echo get_the_content(); ?></p>
+      </div>
+      
+    <?php }
+  ?>
+</section>
 
 <?php get_footer() ?>
