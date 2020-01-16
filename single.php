824fa170d015b53a68d35ca137c7fcbdeda16e3e
Darsh <darsh2987@gmail.com>
2020-01-16 14:06:08 +0000

    While loop added for single posts.

--- single.php
+++ single.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
 
+<?php
+    while(have_posts()) {
+      the_post(); ?>
+        <div>
+          <h2><?php the_title(); ?></h2>
+          <?php the_post_thumbnail(); ?>
+          <?php echo get_the_content(); ?>
+        </div>
+    <?php }
+  ?>
+
 <?php get_header(); ?>
