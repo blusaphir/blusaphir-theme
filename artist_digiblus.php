01ae7d9ee3dfe79ecd3ffa3e9af629ecc5c5f76a
Darsh <darsh2987@gmail.com>
2020-01-18 11:26:20 +0000

    removed and replaced markup with loop.

--- artist_digiblus.php
+++ artist_digiblus.php
@@ -2,180 +2,23 @@
 
 <?php get_header()?>
 
-      <!-- Main Feature -->
-      <h1 class="artist-headline">Artist's - Digital Blu's</h1>
+  <!-- Loop to show all artist posts as a grid gallery -->
+  <section class="artist-grid">
+    <?php 
+      $artistDigiBlus = new WP_Query(array(
+        'posts_per_page' => -1,
+        'post_type' => 'artist_digi_blus'
+      ));
 
-      <section class="artist_digiblus">
-        <div>
-          <p>Cnof</p>
-          <a href=""><img src="assets/images/artists/digi-blus/cnof.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Paul SG</p>
-          <a href=""><img src="assets/images/artists/digi-blus/paul-sg.png" alt=""></a>
-        </div>
-
-        <div>
-          <p>ARTL</p>
-          <a href=""><img src="assets/images/artists/digi-blus/artl.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Velocity</p>
-          <a href=""><img src="assets/images/artists/digi-blus/velocity.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Euphorics</p>
-          <a href=""><img src="assets/images/artists/digi-blus/euphorics.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Sunny Crimea</p>
-          <a href=""><img src="assets/images/artists/digi-blus/sunny-crimea.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Rizla</p>
-          <a href=""><img src="assets/images/artists/digi-blus/rizla.png" alt=""></a>
-        </div>
-
-        <div>
-          <p>Dave Owen</p>
-          <a href=""><img src="assets/images/artists/digi-blus/dave-owen.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Krot</p>
-          <a href=""><img src="assets/images/artists/digi-blus/krot.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>LaMeduza</p>
-          <a href=""><img src="assets/images/artists/digi-blus/lameduza.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Western Sea</p>
-          <a href=""><img src="assets/images/artists/digi-blus/western-sea.png" alt=""></a>
-        </div>
-
-        <div>
-          <p>M Jazzy</p>
-          <a href=""><img src="assets/images/artists/digi-blus/m-jazzy.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>DJ Andrezz</p>
-          <a href=""><img src="assets/images/artists/digi-blus/dj-andrezz.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Sinistarr</p>
-          <a href=""><img src="assets/images/artists/digi-blus/sinistarr.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Nummix</p>
-          <a href=""><img src="assets/images/artists/digi-blus/nummix.png" alt=""></a>
-        </div>
-
-        <div>
-          <p>Simplification</p>
-          <a href=""><img src="assets/images/artists/digi-blus/simplification.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Underdog</p>
-          <a href=""><img src="assets/images/artists/digi-blus/underdog.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>NotioN</p>
-          <a href=""><img src="assets/images/artists/digi-blus/notion.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Skyweep</p>
-          <a href=""><img src="assets/images/artists/digi-blus/skyweep.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>DJ Clart</p>
-          <a href=""><img src="assets/images/artists/digi-blus/dj-clart.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Rowpieces</p>
-          <a href=""><img src="assets/images/artists/digi-blus/rowpieces.png" alt=""></a>
-        </div>
-
-        <div>
-          <p>Cutworks</p>
-          <a href=""><img src="assets/images/artists/digi-blus/cutworks.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Kasper</p>
-          <a href=""><img src="assets/images/artists/digi-blus/kasper.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Flowrian</p>
-          <a href=""><img src="assets/images/artists/digi-blus/flowrian.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Loz Contreras</p>
-          <a href=""><img src="assets/images/artists/digi-blus/loz-contreras.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Capsika</p>
-          <a href=""><img src="assets/images/artists/digi-blus/capsika.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Edward Oberon</p>
-          <a href=""><img src="assets/images/artists/digi-blus/edward-oberon.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Cybass</p>
-          <a href=""><img src="assets/images/artists/digi-blus/cybass.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>The Ego</p>
-          <a href=""><img src="assets/images/artists/digi-blus/the-ego.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Furney</p>
-          <a href=""><img src="assets/images/artists/digi-blus/furney.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>DJ Roots</p>
-          <a href=""><img src="assets/images/artists/digi-blus/dj-roots.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>DJ Chap</p>
-          <a href=""><img src="assets/images/artists/digi-blus/dj-chap.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Jay Rome</p>
-          <a href=""><img src="assets/images/artists/digi-blus/jay-rome.jpg" alt=""></a>
-        </div>
-
-        <div>
-          <p>Kjell</p>
-          <a href=""><img src="assets/images/artists/digi-blus/kjell.jpg" alt=""></a>
-        </div>
-      </section>
-      <!-- End of Main Feature -->
+      while($artistDigiBlus->have_posts()) {
+        $artistDigiBlus->the_post(); ?>
+          <div>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>  
+            <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          </div>
+      <?php }
+    ?>
+  </section>
+  <!-- End of loop to show all artist posts as a grid gallery -->    
 
 <?php get_footer()?>
