e1cafb253ce67103385857be32039e901c32b78c
Darsh <darsh2987@gmail.com>
2020-01-18 17:19:17 +0000

    loop added to get the page title.

--- artist_digiblus.php
+++ artist_digiblus.php
@@ -2,23 +2,34 @@
 
 <?php get_header()?>
 
-  <!-- Loop to show all artist posts as a grid gallery -->
-  <section class="artist-grid">
-    <?php 
-      $artistDigiBlus = new WP_Query(array(
-        'posts_per_page' => -1,
-        'post_type' => 'artist_digi_blus'
-      ));
+<!-- Loop to get the page title -->
+<div class="page-title">
+  <?php
+    while(have_posts()) {
+      the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+    <?php }
+  ?>
+</div>
+<!-- End of Loop to get the page title -->
 
-      while($artistDigiBlus->have_posts()) {
-        $artistDigiBlus->the_post(); ?>
-          <div>
-            <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-          </div>
-      <?php }
-    ?>
-  </section>
-  <!-- End of loop to show all artist posts as a grid gallery -->    
+<!-- Loop to show all artist posts as a grid gallery -->
+<section class="artist-grid">
+  <?php 
+    $artistDigiBlus = new WP_Query(array(
+      'posts_per_page' => -1,
+      'post_type' => 'artist_digi_blus'
+    ));
+
+    while($artistDigiBlus->have_posts()) {
+      $artistDigiBlus->the_post(); ?>
+        <div>
+          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+        </div>
+    <?php }
+  ?>
+</section>
+<!-- End of loop to show all artist posts as a grid gallery -->    
 
 <?php get_footer()?>
