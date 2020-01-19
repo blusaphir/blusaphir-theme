da9217f29b9b1b5e3b6cb423e2c33186bceb64e3
Darsh <darsh2987@gmail.com>
2020-01-19 15:38:09 +0000

    loop modified with conditional statement in case a second/different image is needed.

--- single.php
+++ single.php
@@ -1,12 +1,30 @@
 <?php get_header(); ?>
 
-<?php
+  <?php
     while(have_posts()) {
       the_post(); ?>
         <div>
           <h2><?php the_title(); ?></h2>
-          <?php the_post_thumbnail(); ?>
-          <?php echo get_the_content(); ?>
+
+          <?php
+            $args = array( 
+            'post_type' => 'attachment', 
+            'post_mime_type' => 'image',
+            'numberposts' => -1, 
+            'post_status' => null, 
+            'post_parent' => $post->ID 
+          ); 
+
+          $attached_images = get_posts( $args );
+
+          if ($attached_images) {
+            echo get_the_content();
+          } else if (!$attached_images) {
+            the_post_thumbnail();
+            echo get_the_content();
+          }
+
+          ?>
         </div>
     <?php }
   ?>
