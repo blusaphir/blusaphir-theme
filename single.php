53ac710a6571e7af5c0c697e8a52edc4e2991f1e
Darsh <darsh2987@gmail.com>
2020-01-19 18:28:04 +0000

    logic added to display different images if post content image exists(not featured image).

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
   <?php
     while(have_posts()) {
       the_post(); ?>
-        <div class="sinle-posts">
+        <div class="single-posts">
           <h2><?php the_title(); ?></h2>
 
           <?php
@@ -13,16 +13,36 @@
             'numberposts' => -1, 
             'post_status' => null, 
             'post_parent' => $post->ID 
-          ); 
+          );
 
           $attached_images = get_posts( $args );
+          
+          if ($attached_images) { ?>
 
-          if ($attached_images) {
-            echo get_the_content();
-          } else if (!$attached_images) {
-            the_post_thumbnail();
-            echo get_the_content();
-          }
+          <!-- This is to get and display the image ONLY from the post content -->
+          <?php 
+            preg_match_all('/(<img [^>]*>)/', get_the_content(), $images);
+            for($i=0; isset($images[1]) && $i < count($images[1]); $i++) {
+                echo $images[1][$i];
+            }
+          ?>
+          
+          <!-- This removes the image from the post content allowing ONLY to display the text -->
+          <p>
+            <?php 
+              $content = get_the_content();
+              $content = preg_replace("/<img[^>]+\>/i", " ", $content);          
+              $content = apply_filters('the_content', $content);
+              $content = str_replace(']]>', ']]>', $content);
+              echo $content;
+            ?>
+          </p>
+          
+          <!-- If there is no image within the content then the "featured" image will be displayed with the rest of the content -->
+          <?php } else if (!$attached_images) {
+            the_post_thumbnail(); ?>
+            <p><?php echo get_the_content(); ?></p>
+          <?php }
 
           ?>
         </div>
