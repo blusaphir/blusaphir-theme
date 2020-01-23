cd553509ab002870277e2cd2f074e51325b2d485
Darsh <darsh2987@gmail.com>
2020-01-23 18:26:45 +0000

    footer page restructured with social nav and external links.

--- footer.php
+++ footer.php
@@ -1,22 +1,37 @@
 
-      <!-- Social -->
-      <div class="social-links">
+    
+</div> <!-- End of Wrapper -->
+
+    <!-- Footer -->
+    <footer>
+      <div class="footer-nav-links">
         <ul>
-          <li class="facebook"><a href="<?php echo 'https://www.facebook.com/blusaphirrecs/?ref=nf'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/facebook.png" alt="Facebook"></a></li>
-          <li><a href="<?php echo 'https://www.youtube.com/user/jayrome1978'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/youtube.png" alt="YouTube"></a></li>
-          <li><a href="<?php echo 'https://soundcloud.com/blusaphir_records'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/soundcloud.png" alt="Sound Cloud"></a></li>
-          <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/twitter.jpg" alt="Twitter"></a></li>
-          <li><a href="<?php echo 'https://www.blogger.com/profile/09110695010939377220'; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/social-links/blogger.png" alt="Blogger"></a></li>
+          <li><a href="<?php echo site_url(); ?>">home</a></li>
+          <li><a href="<?php echo site_url('/artists'); ?>">artists</a></li>
+          <li><a href="<?php echo site_url('/events'); ?>">events</a></li>
+          <li><a href="<?php echo site_url('/radio'); ?>">radio</a></li>
+          <li><a href="<?php echo site_url('/dubplates'); ?>">dubplates</a></li>
+          <li><a href="<?php echo site_url('/release'); ?>">release</a></li>
+        </ul>
+
+        <ul>
+          <li><a href="<?php echo site_url('/about'); ?>">about</a></li>
+          <li><a href="<?php echo site_url('/contact'); ?>">contact</a></li>
+        </ul>
+
+        <ul>
+          <li><a href="<?php echo 'https://www.facebook.com/blusaphirrecs/?ref=nf'; ?>">Facebook</a></li>
+          <li><a href="<?php echo 'https://www.youtube.com/user/jayrome1978'; ?>">Youtube</a></li>
+          <li><a href="<?php echo 'https://soundcloud.com/blusaphir_records'; ?>">Soundcloud</a></li>
+          <li><a href="<?php echo 'https://twitter.com/blusaphir_recs?lang=en-gb'; ?>">Twitter</a></li>
+          <li><a href="<?php echo 'https://www.blogger.com/profile/09110695010939377220'; ?>">Blogger</a></li>
         </ul>
       </div>
 
-      <!-- Footer -->
-      <footer>
-        <p>All content © Blu Saphir Recordings Limited 2019. All Rights Reserved.</p>
-        <p>Website designed and developed by Darshan Patel and Jason Williams.</p>
-      </footer>
-      <!-- End of Footer -->
-    </div> <!-- End of Wrapper -->
+      <p>2019© Blu Saphir Recordings Limited.</p>
+      <p>Website designed and developed by Darshan Patel and Jason Williams.</p>
+    </footer>
+    <!-- End of Footer -->
 
     <?php wp_footer(); ?>
   </body>
