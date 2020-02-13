a4be6646ffddc7cb89f04a794ccdcacc42bc2dfe
Darsh <darsh2987@gmail.com>
2020-02-13 22:42:29 +0000

    about page updated and funtion to remove unwanted custome post types added.

--- about.php
+++ about.php
@@ -2,16 +2,23 @@
 
 <?php get_header() ?>
 
-<!-- Loop to show Posts from the About Page -->
+<!-- Loop to show "About" Posts on the About Page -->
 <section class="about">
-  <?php
-    while(have_posts()) {
-      the_post(); ?>
+  <?php 
+    $aboutPosts = new WP_Query(array(
+      'post_type' => 'post',
+      'post_status' => 'publish',
+      'posts_per_page' => -1,
+      'category_name' => 'about'
+    ));
 
-      <div class="content-wrapper">
-        <p><?php echo get_the_content(); ?></p>
-      </div>
-      
+    while($aboutPosts->have_posts()) {
+      $aboutPosts->the_post(); ?>
+        <div class="content-wrapper">
+          <h2><?php the_title(); ?></h2>
+          <?php the_post_thumbnail(); ?>
+          <p><?php echo get_the_content(); ?></p>
+        </div>
     <?php }
   ?>
 </section>
