3935c0bcb720954308c35b7dd6b5f6cff1d1a79b
Darsh <darsh2987@gmail.com>
2020-02-15 15:16:47 +0000

    artist blu saphir ltd page created with markup and php post loop.

---
+++ artist_blusaphirltd.php
@@ -0,0 +1,36 @@
+<?php /* Template Name: Artists - Blu Saphir Ltd */?>
+
+<?php get_header()?>
+
+<!-- Loop to get the page title -->
+<div class="page-title">
+  <?php
+    while(have_posts()) {
+      the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+    <?php }
+  ?>
+</div>
+<!-- End of Loop to get the page title -->
+
+<!-- Loop to show all artist posts as a grid gallery -->
+<section class="artist-grid">
+  <?php 
+    $artistDigiBlus = new WP_Query(array(
+      'post_type' => 'post',
+      'posts_per_page' => -1,
+      'category_name' => 'artists-blu-saphir-ltd'
+    ));
+
+    while($artistDigiBlus->have_posts()) {
+      $artistDigiBlus->the_post(); ?>
+        <div>
+          <h2 class='highlight'><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+        </div>
+    <?php }
+  ?>
+</section>
+<!-- End of loop to show all artist posts as a grid gallery -->    
+
+<?php get_footer()?>
