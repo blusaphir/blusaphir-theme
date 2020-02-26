352887621baeb58f0212fb34e964a05aab60ed4e
Darsh <darsh2987@gmail.com>
2020-02-26 22:36:28 +0000

    added php mark up to loop thorugh post tags to display image related to that tag.

--- single.php
+++ single.php
@@ -9,6 +9,17 @@
           <h2><?php the_title(); ?></h2>
           <?php echo get_the_content(); ?>
           <?php the_tags(); ?>
+
+          <?php
+            $posttags = get_the_tags();
+            if ($posttags) {
+              foreach($posttags as $tag) {
+                the_post_thumbnail(); 
+              }
+            }
+          ?>
+
+          
           </div>
         </div>
     <?php }
