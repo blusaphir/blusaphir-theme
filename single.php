a92f471538bc76040fdcb1eda0dfcdcaf9943c5a
Darsh <darsh2987@gmail.com>
2020-03-23 17:38:41 +0000

    single.php adjusting php markup - tag section, only to show post tags and NOT all tags.

--- single.php
+++ single.php
@@ -6,36 +6,34 @@
       the_post(); ?>
         <div class="single-posts">
           <div class="single-posts-body">
-            <h2><?php the_title(); ?></h2>
-            <?php echo get_the_content(); ?>
+          <h2><?php the_title(); ?></h2>
+          <?php echo get_the_content(); ?>
 
-            <?php
-            if(has_tag()) {
-              $tags = the_tags();
-              $postId = get_the_ID();
+          <?php
+            $tags = get_tags();
+            $postId = get_the_ID();
 
-              foreach($tags as $tag) {
-                $args = array(
-                  'tag__in' => array($tag->term_id),
-                  'caller_get_posts' => 1
-                );
+            foreach($tags as $tag) {
+              $args = array(
+                'tag__in' => array($tag->term_id),
+                'caller_get_posts' => 1
+              );
 
-                $my_query = new WP_Query($args);
-                if($my_query->have_posts()) {
-                  while ($my_query->have_posts()) : $my_query->the_post();
-                  $id = get_the_ID();
+              $my_query = new WP_Query($args);
+              if($my_query->have_posts()) {
+                while ($my_query->have_posts()) : $my_query->the_post();
+                $id = get_the_ID();
 
-                  if ($id !== $postId) { ?>
-                    <div class="tag"><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2><br /><?php the_post_thumbnail(); ?></a></div>
-                  <?php }; ?>
-                <?php endwhile;
-                }
+                if ($id !== $postId) { ?>
+                  <div class="tag"><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2><br /><?php the_post_thumbnail(); ?></a></div>
+                <?php }; ?>
+
+              <?php endwhile;
               }
-            } else {
+            }
+          ?>
 
-            };
-            
-            ?>
+          
           </div>
         </div>
     <?php }
