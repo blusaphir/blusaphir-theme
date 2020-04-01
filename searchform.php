9c086e3b67cd8d47c0b5d62a19eea0a967af5e43
Darsh <darsh2987@gmail.com>
2020-04-01 18:39:46 +0100

    search from notworking - not retunring any results.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form class="search" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
   <input class="search-box" type="text" name="s" placeholder="Search" value="<?php the_search_query(); ?>">
-  <input class="search-icon" type="image" src="<?php echo get_template_directory_uri(); ?>/images/search-icon.png">
+  <input class="search-icon" type="submit" src="<?php echo get_template_directory_uri(); ?>/images/search-icon.png">
 </form>
 
