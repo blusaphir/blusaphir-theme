175e6904a7a287e86e3080563ae4a37744907d58
Darsh <darsh2987@gmail.com>
2020-04-01 19:12:53 +0100

    search from notworking - trying to fix.

--- searchform.php
+++ searchform.php
@@ -1,12 +1,5 @@
-<!-- <form class="search" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+<form class="search" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
   <input class="search-box" type="text" name="s" placeholder="Search" value="<?php the_search_query(); ?>">
   <input class="search-icon" type="image" src="<?php echo get_template_directory_uri(); ?>/images/search-icon.png">
-</form>-->
-
-
-<form action=<?php echo esc_url(home_url('/')); ?> method="get" class="search">
-    <label for="search"></label>
-    <input class="search-box" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-    <input class="search-icon" type="image" alt="Search" src="<?php echo get_template_directory_uri(); ?>/images/search-icon.png" />
 </form>
 
