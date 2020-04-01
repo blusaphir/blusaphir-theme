d222b39d45938adc5dd405ce437fb88d139a1f84
Darsh <darsh2987@gmail.com>
2020-04-01 18:25:51 +0100

    fixing search form.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="search" role="search" method="get" action="<?php echo home_url('/'); ?>">
-  <input class="search-box" type="text" name="s" placeholder="Search" value="">
+  <input class="search-box" type="text" name="s" placeholder="Search" value="<?php echo get_search_query(); ?>">
 </form>
 
