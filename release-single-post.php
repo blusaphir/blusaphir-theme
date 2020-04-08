80836431dda740e4d1e64c0cd482eec2e2ce37d2
Darsh <darsh2987@gmail.com>
2020-04-08 14:43:38 +0100

    revert

--- release-single-post.php
+++ release-single-post.php
@@ -33,7 +33,7 @@
                         while ($my_query->have_posts()) : $my_query->the_post();
                         $id = get_the_ID();
         
-                        if ($id !== $postId && $id !== $id) { ?>
+                        if ($id !== $postId) { ?>
                             <div class="tag"><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2><br /><?php the_post_thumbnail(); ?></a></div>
                         <?php }; ?>
         
