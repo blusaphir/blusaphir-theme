98e2368b6cd178b2276ef72e1be3f297d6b605d4
Jason Williams <jase.williams@gmail.com>
2020-08-04 14:17:25 +0100

    make background change with css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 import "../scss/imports.scss";
 
-window.addEventListener("load", function() {
-
+window.addEventListener("load", function () {
   //Function for the mobile view search icon to open the search box
   function toggleMobileSearchBox() {
-    document.querySelector(".search-icon-mobile").addEventListener("click", toggle);
+    document
+      .querySelector(".search-icon-mobile")
+      .addEventListener("click", toggle);
     function toggle() {
       const mobileSearchBox = document.querySelector(".mobile-search-box");
 
       if (mobileSearchBox.classList.contains("mobile-search-box--show")) {
         mobileSearchBox.classList.remove("mobile-search-box--show");
-      } else { 
+      } else {
         mobileSearchBox.classList.add("mobile-search-box--show");
       }
     }
@@ -33,98 +34,82 @@ window.addEventListener("load", function() {
     }
   }
 
-  const container = document.querySelector('.front-page-random-artist-container, .artist-grid');
-
-  container.addEventListener('mouseover', e => {
-    if (e.target.classList.contains('wp-post-image')) {
-      const target = e.target.parentElement.previousElementSibling;
-      target.style.backgroundColor = "#0E546B";
-    }
-  });
-
-  container.addEventListener('mouseout', e => {
-    if (e.target.classList.contains('wp-post-image')) {
-      const target = e.target.parentElement.previousElementSibling;
-      target.style.backgroundColor = "#1ca9d6";
-    }
-  });
-
-
   //Function to generate and display social icons for the external links such as Facebook, Twitter etc.
   function generateSocialIcons() {
-    const postATags = document.querySelectorAll(".artist-single-posts-body a, .release-single-posts-body p a, .footer-nav-links-social a");
+    const postATags = document.querySelectorAll(
+      ".artist-single-posts-body a, .release-single-posts-body p a, .footer-nav-links-social a"
+    );
 
-    postATags.forEach(function(aTag) {
-      const newSpan = document.createElement('span');
-      newSpan.classList.add('social-icons');
+    postATags.forEach(function (aTag) {
+      const newSpan = document.createElement("span");
+      newSpan.classList.add("social-icons");
 
       switch (aTag.innerText.toLowerCase()) {
         case "facebook":
         case "facebook - blu saphir recs":
         case "facebook - digital blus":
         case "facebook - blu saphir ltd":
-          newSpan.classList.add('facebook-icon');
+          newSpan.classList.add("facebook-icon");
           aTag.prepend(newSpan);
           break;
         case "soundcloud":
-          newSpan.classList.add('soundcloud-icon');
+          newSpan.classList.add("soundcloud-icon");
           aTag.prepend(newSpan);
           break;
         case "twitter":
-          newSpan.classList.add('twitter-icon');
+          newSpan.classList.add("twitter-icon");
           aTag.prepend(newSpan);
           break;
         case "instagram":
-          newSpan.classList.add('instagram-icon');
+          newSpan.classList.add("instagram-icon");
           aTag.prepend(newSpan);
           break;
         case "mixcloud":
-          newSpan.classList.add('mixcloud-icon');
+          newSpan.classList.add("mixcloud-icon");
           aTag.prepend(newSpan);
           break;
         case "beatport":
         case "beatport - blu saphir":
         case "beatport - digital blus":
         case "beatport - blu saphir ltd":
-          newSpan.classList.add('beatport-icon');
+          newSpan.classList.add("beatport-icon");
           aTag.prepend(newSpan);
           break;
         case "spotify":
-          newSpan.classList.add('spotify-icon');
+          newSpan.classList.add("spotify-icon");
           aTag.prepend(newSpan);
           break;
         case "patreon":
-          newSpan.classList.add('patreon-icon');
+          newSpan.classList.add("patreon-icon");
           aTag.prepend(newSpan);
           break;
         case "discogs":
-          newSpan.classList.add('discogs-icon');
+          newSpan.classList.add("discogs-icon");
           aTag.prepend(newSpan);
           break;
         case "bandcamp":
-          newSpan.classList.add('bandcamp-icon');
+          newSpan.classList.add("bandcamp-icon");
           aTag.prepend(newSpan);
           break;
         case "youtube":
-          newSpan.classList.add('youtube-icon');
+          newSpan.classList.add("youtube-icon");
           aTag.prepend(newSpan);
           break;
-      } 
-    })
+      }
+    });
   }
 
   // Function to get current year and dynamically keep the footer date year upto date
   function getyear() {
     const date = new Date();
     const year = date.getFullYear();
-    document.querySelector("#copyright").innerHTML = ` ${year}&copy Blu Saphir Recordings Limited.`
-  };
-
+    document.querySelector(
+      "#copyright"
+    ).innerHTML = ` ${year}&copy Blu Saphir Recordings Limited.`;
+  }
 
   toggleMobileSearchBox();
   hamburger();
   generateSocialIcons();
   getyear();
-
-
 });
